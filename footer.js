27281ad0a34148cb8d82020d5853a1e350d69e49
ravi258g <ravi258g@gmail.com>
2026-03-31 19:48:40 +0530

    Update footer 310326

--- footer.js
+++ footer.js
@@ -4,8 +4,8 @@ document.write(`
   <p>
     <a href="https://instagram.com/kamalini_sarees" target="_blank" rel="noopener noreferrer">
       Instagram
-    </a> •
-    <a href="https://wa.me/916281353964" target="_blank" rel="noopener noreferrer">
+    </a>
+    <a href="https://wa.me/916281353964?text=Hi%20Kamalini%2C%20I%20have%20a%20query." target="_blank" rel="noopener noreferrer">
       WhatsApp +91-6281353964
     </a>
   </p>
