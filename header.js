bb60b19cbf8f0882513c9d5826401beb2b29ade3
ravi258g <ravi258g@gmail.com>
2026-03-31 19:49:37 +0530

    header.js update 310326

--- header.js
+++ header.js
@@ -12,12 +12,12 @@ document.write(`
       <li><a href="new-arrivals.html">New Arrivals</a></li>
       <li><a href="about.html">About</a></li>
       <li>
-        <a href="https://wa.me/916281353964?text=Hi%20Kamalini%2C%20I%20have%20a%20query."
-           target="_blank"
-           rel="noopener noreferrer">
-          Contact
-        </a>
-      </li>
+		<a href="https://wa.me/916281353964?text=Hi%20Kamalini%2C%20I%20have%20a%20query."
+			target="_blank"
+			rel="noopener noreferrer">
+			Contact
+		</a>
+		</li>
     </ul>
   </nav>
 </header>
